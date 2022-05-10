f05709600cd00053fba98d61ffd5f7b8fd05925a
idan.b@heilasystems.com <Liamimri121212>
2022-05-10 11:34:17 +0300

    ver 1

--- routers.go
+++ routers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,16 @@ func GetHello(c *gin.Context) {
 
 func GitPass(c *gin.Context) {
 	c.JSON(200, gin.H{
+		"message": "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",
+	})
+}
+
+func Test1(c *gin.Context) {
+	fmt.Println(c.ClientIP())
+	fmt.Println(c.FullPath())
+
+	c.JSON(200, gin.H{
+		"status":  "ok",
 		"message": "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",
 	})
 }
